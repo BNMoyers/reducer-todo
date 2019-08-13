b7292c3ca4f83860413eaa427f0dc5b0f2db4629
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-08-13 14:53:05 -0500

    converted app class to function

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,73 +1,56 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
+import { initialState, todoReducer } from './reducers/Reducer'
 import TodoForm from './components/TodoComponents/TodoForm';
 import TodoList from './components/TodoComponents/TodoList';
-
 import './components/TodoComponents/Todo.css';
 
-const listData = [
-  {
-    task: 'finish app',
-    id: 1111111,
-    completed: false
-  }
+
+const  App = (props) => {
   
-]
-class App extends React.Component {
-  constructor() {
-  super();
-  this.state = {
-    todos: listData
-  };
-}
+  return (
+    <div className = 'app'>
+      <h1>Get SHIFT Done</h1>
+      <TodoList />
+      <TodoForm />
+    </div>
+  );
 
-toggleTask = id => {
-  this.setState({
-todos: this.state.todos.map(task => {
-      if (task.id === id) {
-         return {
-          ...task,
-          completed: !task.completed
-         };
-    } else {
-        return task;
-      }
-    })
-  });
-};
+// toggleTask = id => {
+//   this.setState({
+// todos: this.state.todos.map(task => {
+//       if (task.id === id) {
+//          return {
+//           ...task,
+//           completed: !task.completed
+//          };
+//     } else {
+//         return task;
+//       }
+//     })
+//   });
+// };
 
-addTask = taskName => {
-  const newTask = {
-    task: taskName,
-    id: Date.now(),
-    completed: false
-  }
-  this.setState({
-    todos: [...this.state.todos, newTask]
-  })
-}
+// addTask = taskName => {
+//   const newTask = {
+//     task: taskName,
+//     id: Date.now(),
+//     completed: false
+//   }
+//   this.setState({
+//     todos: [...this.state.todos, newTask]
+//   })
+// }
 
-clearCompleted = () => {
-  this.setState({
-    todos: this.state.todos.filter(task => !task.completed)
-  })
+// clearCompleted = () => {
+//   this.setState({
+//     todos: this.state.todos.filter(task => !task.completed)
+//   })
  
-}
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+
 
   
-  render() {
-    return (
-      <div className = 'app'>
-        <h1>Get SHIFT Done</h1>
-        <TodoList todos={this.state.todos}
-                  toggleTask={this.toggleTask} 
-                  clearCompleted={this.clearCompleted}/>
-        <TodoForm addTask={this.addTask}/>
-      </div>
-    );
-  }
+   
+  
 }
 
 export default App;
